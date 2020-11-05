d9bf648b6df95b656730465a90c26d5b504a8dba
jonzamora <jonzamora18@gmail.com>
2020-11-05 02:02:44 -0800

    Part 2 Updates

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,13 +18,20 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loadTweets()
-        
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         
-        tableView.refreshControl = myRefreshControl
-        tableView.refreshControl?.tintColor = UIColor.white
+        self.tableView.refreshControl = myRefreshControl
+        self.tableView.refreshControl?.tintColor = UIColor.white
         
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
     }
     
     
@@ -102,6 +109,12 @@ class HomeTableViewController: UITableViewController {
         
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
+        }
+        
+        if #available(iOS 13.0, *) {
+            cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+            cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+            cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         }
         
         return cell
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -14,6 +14,99 @@ class TweetCellTableViewCell: UITableViewCell {
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var tweetContent: UILabel!
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    var retweeted:Bool = false
+    
+    @available(iOS 13.0, *)
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        
+        let heartFill = UIImage(systemName: "heart.fill")?.withTintColor(UIColor.red, renderingMode: .alwaysOriginal)
+        let heartNoFill = UIImage(systemName: "heart")
+        
+        if (favorited) {
+            favButton.setImage(heartFill, for: UIControl.State.normal)
+        }
+        
+        else {
+            favButton.setImage(heartNoFill, for: UIControl.State.normal)
+        }
+    }
+    
+    @available(iOS 13.0, *)
+    func setRetweeted(_ isRetweeted:Bool) {
+        retweeted = isRetweeted
+        
+        let retweetFill = UIImage(systemName: "arrow.left.arrow.right")?.withTintColor(UIColor.green, renderingMode: .alwaysOriginal)
+        let retweetNoFill = UIImage(systemName: "arrow.left.arrow.right")
+        
+        if (retweeted) {
+            retweetButton.setImage(retweetFill, for: UIControl.State.normal)
+            // retweetButton.isEnabled = false
+        }
+        
+        else {
+            retweetButton.setImage(retweetNoFill, for: UIControl.State.normal)
+            // retweetButton.isEnabled = true
+        }
+    }
+    
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                if #available(iOS 13.0, *) {
+                    self.setFavorite(true)
+                }
+            }, failure: { (error) in
+                print("Favorite did not succeed: \(error)")
+            })
+        }
+        
+        else
+        {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                if #available(iOS 13.0, *) {
+                    self.setFavorite(false)
+                }
+            }, failure: { (error) in
+                print("Unfavorite did not succeed: \(error)")
+            })
+        }
+    }
+    
+    
+    @IBAction func retweet(_ sender: Any) {
+        let toBeRetweeted = !retweeted
+        
+        if (toBeRetweeted) {
+            TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+                if #available(iOS 13.0, *) {
+                    self.setRetweeted(true)
+                }
+            }, failure: { (error) in
+                print("Error is retweeting: \(error)")
+            })
+        }
+        
+        else {
+            TwitterAPICaller.client?.unRetweet(tweetId: tweetId, success: {
+                if #available(iOS 13.0, *) {
+                    self.setRetweeted(false)
+                }
+            }, failure: { (error) in
+                print("Error is retweeting: \(error)")
+            })
+        }
+        
+    }
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
